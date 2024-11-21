faa57bb2ededb9e76a9d9b3590037cff2cda195b
Yosip Mike Colin <vendeyaonline10@gmail.com>
2024-11-21 14:17:24 -0500

    Correction limit of attribute

--- src/components/dashboard/views/attributes/Attributes.tsx
+++ src/components/dashboard/views/attributes/Attributes.tsx
@@ -4,17 +4,25 @@ import { useRef, useState } from "react";
 import { AttributeUpdated } from "@/interfaces";
 import { TableAttribute } from "../../components/tables";
 import { ModalAttribute } from "../../components/modals";
+import { useQueryAttribute } from "@/api/queries";
 
 const Attributes = () => {
   const [activeModal, setActiveModal] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [search, setSearch] = useState("");
+  const { data } = useQueryAttribute(currentPage, search);
   const selectedItem = useRef<AttributeUpdated | undefined>(undefined);
   return (
     <section>
       <div className="mb-5">
-        <Button onClik={() => setActiveModal(true)}>
-          Crear atributo <PencilRuler size={18} />
+        <Button
+          onClik={() => setActiveModal(true)}
+          disabled={data && data?.grandTotal >= 30}
+        >
+          {data && data?.grandTotal >= 30
+            ? "Límite alcanzado"
+            : "Crear atributo"}{" "}
+          <PencilRuler size={18} />
         </Button>
       </div>
       <ModalAttribute
--- src/components/dashboard/views/products/Products.tsx
+++ src/components/dashboard/views/products/Products.tsx
@@ -4,18 +4,26 @@ import { ModalProduct } from "../../components/modals";
 import TableProducts from "../../components/tables/table-products/TableProducts";
 import { ProductTable } from "@/interfaces";
 import { Package } from "lucide-react";
+import { useQueryProducts } from "@/api/queries";
 
 const Products = () => {
   const [activeModal, setActiveModal] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [search, setSearch] = useState("");
+  const { data } = useQueryProducts(currentPage, search);
   const selectedItem = useRef<ProductTable | undefined>(undefined);
 
   return (
     <section>
       <div className="mb-5">
-        <Button onClik={() => setActiveModal(true)}>
-          Crear producto <Package size={18} />
+        <Button
+          onClik={() => setActiveModal(true)}
+          disabled={data && data?.grandTotal >= 100}
+        >
+          {data && data?.grandTotal >= 100
+            ? "Límite alcanzado"
+            : "Crear producto"}
+          <Package size={18} />
         </Button>
       </div>
 
